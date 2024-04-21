d8cc7e9765235dcc0b8cc8a0ca7b3d6620403653
Admin user <adminuser@admin.local>
2024-04-21 16:24:30 +0700

    Add base classes

---
+++ Che.Result/Result.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Che.Result;
+public sealed class Result<T>
+    where T : class
+{
+    public T? Value { get; }
+    public Error Error { get; }
+    [MemberNotNullWhen(true, nameof(Value))]
+    public bool IsSuccess => Error.IsNone;
+    [MemberNotNullWhen(false, nameof(Value))]
+    public bool IsFailure => !IsSuccess;
+
+    private Result(T? value, Error error)
+    {
+        if (value is null && error.IsNone)
+        {
+            throw new ArgumentException("Value and Error cannot both be null");
+        }
+
+        if (value is not null && !error.IsNone)
+        {
+            throw new ArgumentException("Value and Error cannot both be non-null");
+        }
+
+        Value = value;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value) => new(value, Error.None);
+    public static Result<T> Failure(Error error) => new(null, error);
+}
