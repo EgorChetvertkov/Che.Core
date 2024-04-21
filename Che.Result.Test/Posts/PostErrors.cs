4f414e3292c80e280303c4645f5e613c502efb7d
Admin user <adminuser@admin.local>
2024-04-21 17:09:59 +0700

    Add tests

---
+++ Che.Result.Test/Posts/PostErrors.cs
@@ -0,0 +1,13 @@
+﻿namespace Che.Result.Test.Posts;
+public static class PostErrors
+{
+    public static Error TitleAreEmpty => new(
+        nameof(TitleAreEmpty),
+        "Title are empty");
+    public static Error BodyAreEmpty => new(
+        nameof(BodyAreEmpty),
+        "Body are empty");
+    public static Error CreateAtInvalid => new(
+        nameof(CreateAtInvalid),
+        "Post created in the feature");
+}
